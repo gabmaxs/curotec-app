0329c3637ddc1232a1fc248ae18b9a81e0fedce9
Gabriel Maximiniano <gabmaxsan@gmail.com>
2024-12-28 15:14:55 -0300

    add: footer

--- src/modules/room/pages/Room/Room.vue
+++ src/modules/room/pages/Room/Room.vue
@@ -1,14 +1,14 @@
 <template>
   <section class="h-screen flex flex-col justify-start">
-    <nav class="w-full h-auto p-2 flex justify-between">
+    <nav class="w-full h-auto p-2 flex justify-between border-b border-gray-500">
       <Title v-model="pageTitle" />
       <Actions />
     </nav>
     <section class="w-full p-2 flex-1">
       Body
     </section>
-    <footer>
-      <span>Last update: YYYY-MM-DD HH:mm:ss</span>
+    <footer class="flex justify-end border-t border-gray-500">
+      <span class="text-xs">last update: YYYY-MM-DD HH:mm:ss</span>
     </footer>
   </section>
 </template>
