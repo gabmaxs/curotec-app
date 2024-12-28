a6c204caadd7a725a5c5d2f649556ebf133235c3
Gabriel Maximiniano <gabmaxsan@gmail.com>
2024-12-28 15:11:06 -0300

    add: action buttons section

---
+++ src/modules/room/components/Actions/Actions.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="actions">
+    <slot></slot>
+    <button class="button btn-success btn-small">
+      Save
+    </button>
+    <button class="button btn-danger btn-small">
+      Exit
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+#actions {
+  @apply flex items-center gap-2;
+}
+</style>
--- src/modules/room/pages/Room/Room.vue
+++ src/modules/room/pages/Room/Room.vue
@@ -1,3 +1,22 @@
 <template>
-  <div>Room Page</div>
+  <section class="h-screen flex flex-col justify-start">
+    <nav class="w-full h-auto p-2 flex justify-between">
+      <Title v-model="pageTitle" />
+      <Actions />
+    </nav>
+    <section class="w-full p-2 flex-1">
+      Body
+    </section>
+    <footer>
+      <span>Last update: YYYY-MM-DD HH:mm:ss</span>
+    </footer>
+  </section>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import Title from '../../components/Title/Title.vue';
+import Actions from '../../components/Actions/Actions.vue';
+
+const pageTitle = ref<string>('New File');
+</script>
