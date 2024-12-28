6d891e5876c7a7fc38fb2bfa0e1b166c756d1a7e
Gabriel Maximiniano <gabmaxsan@gmail.com>
2024-12-28 14:48:08 -0300

    add: title component

---
+++ src/modules/room/components/Title/Title.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="title">
+    <h2 v-if="!isEdit" @dblclick="handleEditChangeEvent(true)" class="text">{{ model }}</h2>
+    <input
+      v-else
+      v-model="model"
+      @blur="handleEditChangeEvent(false)"
+      @keyup.enter="handleEditChangeEvent(false)"
+      type="text"
+      class="input-text"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const model = defineModel({ required: true });
+const isEdit = ref<boolean>(false);
+
+const handleEditChangeEvent = (value: boolean) => isEdit.value = value;
+</script>
+
+<style scoped>
+.text {
+  @apply cursor-pointer text-xl font-bold;
+}
+.input-text {
+  @apply bg-transparent border border-gray-300;
+}
+</style>
